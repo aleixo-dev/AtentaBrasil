bd0794c06fba5c7f9a48a222e57f6d8212c568ba
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-02-22 20:13:52 -0300

    implement ui components

--- app/src/main/java/br/com/nicolas/atentabrasil/presentation/ddd/DirectDialingViewModel.kt
+++ app/src/main/java/br/com/nicolas/atentabrasil/presentation/ddd/DirectDialingViewModel.kt
@@ -1,7 +1,50 @@
 package br.com.nicolas.atentabrasil.presentation.ddd
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.nicolas.atentabrasil.common.DataState
+import br.com.nicolas.atentabrasil.common.Resource
+import br.com.nicolas.atentabrasil.data.response.Cep
+import br.com.nicolas.atentabrasil.domain.model.DddUiDomain
+import br.com.nicolas.atentabrasil.domain.usecase.FetchDddUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DirectDialingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class DirectDialingViewModel @Inject constructor(
+    private val fetchDddUseCase: FetchDddUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<DirectState>(
+        DirectState.Loading()
+    )
+    val uiState: SharedFlow<DirectState> get() = _uiState
+
+    fun fetchDdd(ddd: String) = viewModelScope.launch {
+        fetchDddUseCase.invoke(ddd).collect { state ->
+            when (state) {
+                is Resource.Loading -> {
+                    _uiState.value = DirectState.Loading(true)
+                }
+                is Resource.Success -> {
+                    _uiState.value = DirectState.Success(state.item)
+                }
+                is Resource.Error -> {
+                    _uiState.value = DirectState.Error(
+                        state.throwable
+                    )
+                }
+            }
+        }
+    }
+
+    sealed class DirectState {
+        data class Loading(val isLoading: Boolean = true) : DirectState()
+        data class Success(val ddd: List<DddUiDomain>) : DirectState()
+        data class Error(val error: String) : DirectState()
+    }
 }
