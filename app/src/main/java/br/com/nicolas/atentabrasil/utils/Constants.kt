2756070b1aee28891ac0920b1e3e65e697e8f138
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-02-22 09:26:55 -0300

    constants

--- app/src/main/java/br/com/nicolas/atentabrasil/utils/Constants.kt
+++ app/src/main/java/br/com/nicolas/atentabrasil/utils/Constants.kt
@@ -3,5 +3,13 @@ package br.com.nicolas.atentabrasil.utils
 object Constants {
 
     const val DATABASE_NAME = ""
+    const val BASE_URL = "https://brasilapi.com.br/"
+
+    // Endpoints
+    const val CEP_ENDPOINT = "api/cep/v1/{cep}"
+    const val DDD_ENDPOINT = "ddd/v1/{ddd}"
+    const val HOLIDAYS_ENDPOINT = "feriados/v1/{ano}"
+    const val FIPE_ENDPOINT_TYPE_VEHICLE = "fipe/marcas/v1/{tipoVeiculo}"
+    const val FIPE_ENDPOINT_CODE = "fipe/preco/v1/{codigoFipe}"
 
 }
