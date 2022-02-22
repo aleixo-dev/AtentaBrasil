d634f13659e11310f6335a54672bf40544a72ccc
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-02-22 09:26:32 -0300

    base fragment and view model

---
+++ app/src/main/java/br/com/nicolas/atentabrasil/domain/usecase/FetchCepUseCase.kt
@@ -0,0 +1,26 @@
+package br.com.nicolas.atentabrasil.domain.usecase
+
+import br.com.nicolas.atentabrasil.common.Resource
+import br.com.nicolas.atentabrasil.data.response.Cep
+import br.com.nicolas.atentabrasil.domain.repository.BrazilRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class FetchCepUseCase @Inject constructor(
+    private val repository: BrazilRepository
+) {
+    operator fun invoke(cep: String): Flow<Resource<Cep>> = flow {
+        try {
+            emit(Resource.Loading(true))
+            val response = repository.fetchCep(cep)
+            emit(Resource.Success(response))
+        } catch (exception: HttpException) {
+            emit(Resource.Error(exception.localizedMessage ?: "An unexpected error occured"))
+        } catch (exception: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
